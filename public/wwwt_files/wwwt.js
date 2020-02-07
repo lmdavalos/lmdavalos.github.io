9141568334f168b48393979e2bb041da7bc272b6
Liliana M Davalos <lmdavalos@gmail.com>
2020-02-07 13:51:53 -0500

    V37

--- public/wwwt_files/wwwt.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20190831
-
-setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
-function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('wwwt_files/wwwtMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');fixAllIEPNGs('Media/transparent.gif');applyEffects()}
