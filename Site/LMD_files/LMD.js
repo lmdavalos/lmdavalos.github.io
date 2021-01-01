0f8ff254a846e7724c79774196e1a6a191dd13d3
Liliana Davalos <lmdavalos@gmail.com>
2021-01-01 10:26:12 -0500

    V60
    Added personal site and GBatNet at last

---
+++ Site/LMD_files/LMD.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20160303
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-6,4,10,469),url:'LMD_files/stroke.png'},{rect:new IWRect(-6,-7,10,11),url:'LMD_files/stroke_1.png'},{rect:new IWRect(4,-6,370,10),url:'LMD_files/stroke_2.png'},{rect:new IWRect(374,-2,10,6),url:'LMD_files/stroke_3.png'},{rect:new IWRect(374,4,8,469),url:'LMD_files/stroke_4.png'},{rect:new IWRect(374,473,8,10),url:'LMD_files/stroke_5.png'},{rect:new IWRect(4,473,370,9),url:'LMD_files/stroke_6.png'},{rect:new IWRect(-5,473,9,13),url:'LMD_files/stroke_7.png'}],new IWSize(377,480))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('LMD_files/LMDMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');IMpreload('LMD_files','shapeimage_1','0');IMpreload('LMD_files','shapeimage_1','1');IMpreload('LMD_files','shapeimage_1','2');IMpreload('LMD_files','shapeimage_1','3');IMpreload('LMD_files','shapeimage_1','4');IMpreload('LMD_files','shapeimage_1','5');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
