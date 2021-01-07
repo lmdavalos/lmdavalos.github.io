5bb956675b054a710a3eb542e3a281aa2192af5f
Liliana Davalos <lmdavalos@gmail.com>
2021-01-06 21:24:00 -0500

    V60.3
    Adding preprint

--- js/wowchemy-init.js
+++ js/wowchemy-init.js
@@ -1,14 +1,18 @@
 (() => {
-  // ns-hugo:/var/folders/lw/jpc4710m8xj6s8001s6nyzx80000gp/T/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy@v0.0.0-20201129124759-cdcfa2da28e2/assets/js/wowchemy-theming.js
+  // ns-hugo:/var/folders/lw/jpc4710m8xj6s8001s6nyzx80000gp/T/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy@v0.0.0-20201228231801-219b05cbd2c4/assets/js/wowchemy-theming.js
+  var body = document.body;
   function getThemeMode() {
-    return parseInt(localStorage.getItem("dark_mode") || 2);
+    return parseInt(localStorage.getItem("wcTheme") || 2);
   }
   function canChangeTheme() {
     return Boolean(window.wc.darkLightEnabled);
   }
   function initThemeVariation() {
     if (!canChangeTheme()) {
-      return;
+      return {
+        isDarkTheme: window.wc.isSiteThemeDark,
+        themeMode: window.wc.isSiteThemeDark ? 1 : 0
+      };
     }
     let currentThemeMode = getThemeMode();
     let isDarkTheme;
@@ -29,11 +33,17 @@
         }
         break;
     }
-    if (isDarkTheme) {
+    if (isDarkTheme && !body.classList.contains("dark")) {
+      console.debug("Applying Wowchemy dark theme");
       document.body.classList.add("dark");
-    } else {
+    } else if (body.classList.contains("dark")) {
+      console.debug("Applying Wowchemy light theme");
       document.body.classList.remove("dark");
     }
+    return {
+      isDarkTheme,
+      themeMode: currentThemeMode
+    };
   }
 
   // ns-params:@params
